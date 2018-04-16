a6e5c02a1591a2130d6e1d97b036d5551b0700d2
isMrH <37281700+isMrH@users.noreply.github.com>
2018-04-16 23:01:00 +0800

    zhihuuser

---
+++ zhihuuser/zhihuuser/items.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy import Item, Field
+
+
+class UserItem(Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    id = Field()
+    name = Field()
+    avatar_url = Field()
+    headline = Field()
+    description = Field()
+    url = Field()
+    url_token = Field()
+    gender = Field()
+    type = Field()
+    badge = Field()
+    answer_count = Field()
+    articles_count = Field()
---
+++ zhihuuser/zhihuuser/spiders/zhihu.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from scrapy import Spider, Request
+import json
+from ..items import UserItem
+
+class ZhihuSpider(Spider):
+    name = 'zhihu'
+    allowed_domains = ['www.zhihu.com']
+    start_urls = ['http://www.zhihu.com/']
+
+    start_user = 'excited-vczh'
+
+    user_url = 'https://www.zhihu.com/api/v4/members/{user}?include={include}'
+    user_query = 'allow_message,is_followed,is_following,is_org,is_blocking,employments,answer_count,follower_count,articles_count,gender,badge[?(type=best_answerer)].topics'
+
+    follow_url = 'https://www.zhihu.com/api/v4/members/{user}/followees?include={include}&limit={limit}&offset={offset}'
+    follow_query = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
+
+    followers_url = 'https://www.zhihu.com/api/v4/members/{user}/followers?include={include}&limit={limit}&offset={offset}'
+    followers_query = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
+    def start_requests(self):
+        url='https://www.zhihu.com/api/v4/members/excited-vczh/relations/mutuals?include=data%5B%2A%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F%28type%3Dbest_answerer%29%5D.topics&limit=10&offset=60'
+        yield Request(self.user_url.format(user=self.start_user, include=self.user_query), self.parse_user)
+        yield Request(self.follow_url.format(user=self.start_user, include=self.follow_query, offset=0, limit=20), callback=self.parse_follows)
+        yield Request(self.followers_url.format(user=self.start_user, include=self.followers_query, offset=0, limit=20), callback=self.parse_followers)
+
+    def parse_user(self, response):
+        result = json.loads(response.text)
+        item = UserItem()
+        for field in item.fields:
+            if field in result.keys():
+                item[field] = result.get(field)
+        yield item
+        yield Request(self.follow_url.format(user=result.get('url_token'), include=self.follow_query, limit=20, offset=0), self.parse_follows)
+        yield Request(self.followers_url.format(user=result.get('url_token'), include=self.followers_query, limit=20, offset=0), self.parse_followers)
+    def parse_follows(self, response):
+        results = json.loads(response.text)
+        if 'data' in results.keys():
+            for result in results.get('data'):
+                yield Request(self.user_url.format(user=result.get('url_token'),include=self.user_query),self.parse_user)
+
+        if 'paging' in results.keys() and results.get('paging').get('is_end') == False:
+            next_page = results.get('paging').get('next')
+            yield Request(next_page, self.parse_follows)
+
+    def parse_followers(self, response):
+        results = json.loads(response.text)
+        if 'data' in results.keys():
+            for result in results.get('data'):
+                yield Request(self.user_url.format(user=result.get('url_token'), include=self.user_query),
+                              self.parse_user)
+
+        if 'paging' in results.keys() and results.get('paging').get('is_end') == False:
+            next_page = results.get('paging').get('next')
+            yield Request(next_page, self.parse_followers)
